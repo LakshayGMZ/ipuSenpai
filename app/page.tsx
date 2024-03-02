407f315256dd738b4e56430a687d4adb61a36e1d
LakshayGMZ <lakshaygulati1898@gmail.com>
2024-03-02 11:29:11 +0530

    added dotted bg

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,14 @@ import React from "react";
 export default function Page() {
 
     return (
-        <div className="relative w-full">
-            <div className={""}>hmm</div>
-        </div>
-    );
+    <div
+        className="inset-0 flex justify-center items-center min-h-screen w-full bg-white bg-[radial-gradient(#e5e7eb_2px,transparent_4px)] [background-size:32px_32px] [mask-image:radial-gradient(ellipse_45%_45%_at_50%_50%,#000_40%,transparent_100%)]"
+    >
+        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+            Backgrounds
+        </p>
+    </div>
+)
+    ;
 }
 
