8e9cb70b95a04db6a68e8557ec9d75f26b955eee
LakshayGMZ <lakshaygulati1898@gmail.com>
2024-03-04 20:45:14 +0530

    integrated api

--- app/(endpoints)/ranklist/page.tsx
+++ app/(endpoints)/ranklist/page.tsx
@@ -1,42 +1,42 @@
 "use client"
 
-import { SelectValue, SelectTrigger, SelectItem, SelectContent, Select } from "@/components/ui/select"
-import { Button } from "@/components/ui/button"
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select"
+import {Button} from "@/components/ui/button"
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
-import {ChevronDownIcon, ChevronsUpDownIcon} from "lucide-react";
+import {ChevronsUpDownIcon} from "lucide-react";
 import {Command, CommandEmpty, CommandInput, CommandItem, CommandList} from "@/components/ui/command";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {getProgrammes} from "@/app/lib/data";
+
 
 export default function Page() {
     const [collegeName, setCollegeName] = useState("ADGIPS");
     const collegeList = ["ADGIPS", "ADGITM", "NIEC", "MAIT", "BPIT", "BVP"]
+
+    useEffect(() => {
+        const programmes = getProgrammes();
+        console.log(programmes);
+    }, []);
+
+
     return (
         <form>
             <div className="bg-[#1a202c] p-[2rem] shadow-md rounded-lg max-w-[72rem] mx-auto my-8">
                 <h1 className="text-4xl font-semibold text-white mb-6">University Ranklist</h1>
                 <div className="grid grid-cols-1 md:grid-cols-7 gap-6">
-                    <Select className="w-full md:col-span-1">
-                        <SelectTrigger id="programme">
-                            <SelectValue placeholder="Programme" />
-                        </SelectTrigger>
-                        <SelectContent position="popper">
-                            <SelectItem value="btech">B. Tech.</SelectItem>
-                            <SelectItem value="mtech">M. Tech.</SelectItem>
-                            <SelectItem value="phd">Ph.D.</SelectItem>
-                        </SelectContent>
-                    </Select>
                     <Popover>
                         <PopoverTrigger asChild>
-                            <Button className="flex h-10 text-black w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1">
+                            <Button
+                                className="flex h-10 text-black w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1">
                                 {collegeName
                                     ? collegeList.find((cllg) => cllg.toLowerCase() === collegeName.toLowerCase())
                                     : "Select College"}
-                                <ChevronsUpDownIcon className="w-4 h-4 translate-x-1" />
+                                <ChevronsUpDownIcon className="w-4 h-4 translate-x-1"/>
                             </Button>
                         </PopoverTrigger>
                         <PopoverContent align="start" className="w-48">
                             <Command>
-                                <CommandInput className="h-9" placeholder="Select College" />
+                                <CommandInput className="h-9" placeholder="Select College"/>
                                 <CommandEmpty>No College found.</CommandEmpty>
                                 <CommandList>
                                     {collegeList.map((cllg) => (
@@ -54,7 +54,27 @@ export default function Page() {
                     </Popover>
                     <Select className="w-full md:col-span-1">
                         <SelectTrigger id="programme">
-                            <SelectValue placeholder="Shift" />
+                            <SelectValue placeholder="Programme"/>
+                        </SelectTrigger>
+                        <SelectContent position="popper">
+                            <SelectItem value="btech">B. Tech.</SelectItem>
+                            <SelectItem value="mtech">M. Tech.</SelectItem>
+                            <SelectItem value="phd">Ph.D.</SelectItem>
+                        </SelectContent>
+                    </Select>
+                    <Select className="w-full md:col-span-1">
+                        <SelectTrigger id="course">
+                            <SelectValue placeholder="Course"/>
+                        </SelectTrigger>
+                        <SelectContent position="popper">
+                            <SelectItem value="cse">CSE</SelectItem>
+                            <SelectItem value="ece">ECE</SelectItem>
+                            <SelectItem value="me">ME</SelectItem>
+                        </SelectContent>
+                    </Select>
+                    <Select className="w-full md:col-span-1">
+                        <SelectTrigger id="programme">
+                            <SelectValue placeholder="Shift"/>
                         </SelectTrigger>
                         <SelectContent position="popper">
                             <SelectItem value="morning">Morning</SelectItem>
@@ -64,7 +84,7 @@ export default function Page() {
                     </Select>
                     <Select className="w-full md:col-span-1">
                         <SelectTrigger id="year">
-                            <SelectValue placeholder="Year Range" />
+                            <SelectValue placeholder="Batch"/>
                         </SelectTrigger>
                         <SelectContent position="popper">
                             <SelectItem value="2022-2026">2022 - 2026</SelectItem>
@@ -72,24 +92,15 @@ export default function Page() {
                             <SelectItem value="2020-2024">2020 - 2024</SelectItem>
                         </SelectContent>
                     </Select>
-                    <Select className="w-full md:col-span-1">
-                        <SelectTrigger id="course">
-                            <SelectValue placeholder="Course" />
-                        </SelectTrigger>
-                        <SelectContent position="popper">
-                            <SelectItem value="cse">CSE</SelectItem>
-                            <SelectItem value="ece">ECE</SelectItem>
-                            <SelectItem value="me">ME</SelectItem>
-                        </SelectContent>
-                    </Select>
+
                     <Select className="w-full md:col-span-1">
                         <SelectTrigger id="criteria">
-                            <SelectValue placeholder="Criteria" />
+                            <SelectValue placeholder="Semester"/>
                         </SelectTrigger>
                         <SelectContent position="popper">
-                            <SelectItem value="overall">Overall</SelectItem>
-                            <SelectItem value="research">Research</SelectItem>
-                            <SelectItem value="teaching">Teaching</SelectItem>
+                            <SelectItem value="overall">Sem 1</SelectItem>
+                            <SelectItem value="research">Sem 2</SelectItem>
+                            <SelectItem value="teaching">Sem 3</SelectItem>
                         </SelectContent>
                     </Select>
                     <Button className="bg-[#38a169] text-white md:col-span-1">Search</Button>
