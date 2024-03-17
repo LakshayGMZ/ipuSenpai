a05b587ad738d86d0684fcf39f5ec597bb7b0950
LakshayGMZ <lakshaygulati1898@gmail.com>
2024-03-17 10:09:56 +0530

    fixed overflow horizontal

--- components/ranklist/StudentDataDialog.tsx
+++ components/ranklist/StudentDataDialog.tsx
@@ -65,7 +65,7 @@ export function StudentDataDialog(
             onOpenChange={handleDrawerClose}
         >
             <DrawerContent>
-                <div className="max-w-4xl mx-auto p-6 shadow rounded">
+                <div className="w-screen md:max-w-4xl mx-auto p-4 md:p-6 shadow rounded">
                     <div className="flex flex-col space-y-4 relative">
                         <div className="grid grid-cols-2 gap-4">
                             <div>
@@ -113,7 +113,7 @@ export function StudentDataDialog(
                             <Button variant="outline">Show credit marks and credit percentage</Button>
                             <Switch id="toggle-details"/>
                         </div>
-                        <div className="relative w-screen md:w-full max-h-[50vh] overflow-auto">
+                        <div className="relative w-full md:w-full max-h-[50vh] overflow-auto">
                             <Table className={"min-w-max"}>
                                 <TableHeader style={{backgroundColor: "hsl(var(--background))"}} className={"sticky top-0"}>
                                     <TableRow>
