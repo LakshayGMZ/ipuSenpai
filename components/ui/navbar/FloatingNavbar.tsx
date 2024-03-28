ab7ee7fea98b5d53df5017ac399313f0a1b7ae83
martian0x80 <26498920+martian0x80@users.noreply.github.com>
2024-03-28 10:06:48 +0530

    Fix floating navbar

--- components/ui/navbar/FloatingNavbar.tsx
+++ components/ui/navbar/FloatingNavbar.tsx
@@ -32,7 +32,7 @@ export const FloatingNav = (
     });
 
     return (
-        <div className={"w-screen h-fit flex fixed top-10"}>
+        <div className={"flex max-w-fit fixed top-10 inset-x-0 mx-auto items-center z-[5000]"}>
             <AnimatePresence mode="wait">
                 <motion.div
                     initial={{
@@ -51,14 +51,13 @@ export const FloatingNav = (
                         "border border-transparent dark:border-white/[0.2] rounded-full " +
                         "dark:bg-black bg-white m-auto " +
                         "shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] " +
-                        "z-[5000] px-4 py-2 whitespace-nowrap ",
-                        className
-                    )}
+                        "px-4 py-2 whitespace-nowrap ",
+                        className)}
                 >
                     <NavbarTabs tabs={navItems} />
                 </motion.div>
             </AnimatePresence>
         </div>
-
     );
+
 };
