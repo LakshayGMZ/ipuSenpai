dc4c150aff4db869e56398f72cf9465135b61b21
LakshayGMZ <lakshaygulati1898@gmail.com>
2024-04-06 16:30:46 +0530

    integrated theme to loader

--- components/ui/Loader.tsx
+++ components/ui/Loader.tsx
@@ -70,7 +70,7 @@ const LoaderCore = (
                                     className={cn(
                                         "text-black dark:text-white",
                                         value === index &&
-                                        "text-black dark:text-lime-500 opacity-100"
+                                        "text-black dark:text-primary opacity-100"
                                     )}
                                 />
                             )}
@@ -78,7 +78,7 @@ const LoaderCore = (
                         <span
                             className={cn(
                                 "text-black dark:text-white",
-                                value === index && "text-black dark:text-lime-500 opacity-100"
+                                value === index && "text-black dark:text-primary opacity-100"
                             )}
                         >
               {loadingState.text}
