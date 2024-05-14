1a52da04aab00bc14267eff0f8aaf2513175e4f0
martian0x80 <26498920+martian0x80@users.noreply.github.com>
2024-05-14 16:10:41 +0530

    Attempt to fix build error

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -15,11 +15,11 @@ Sentry.init({
 
   enabled: process.env.NODE_ENV === "production",
 
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: 0.7,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0.0,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -13,6 +13,8 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
+  enabled: process.env.NODE_ENV === 'production',
+
   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
   
