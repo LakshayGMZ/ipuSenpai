f02b15033684170ad5d02fd86dd658f31a47fc17
LakshayGMZ <lakshaygulati1898@gmail.com>
2024-06-07 10:37:37 +0530

    ad scripts

--- components/ui/CustomScripts.tsx
+++ components/ui/CustomScripts.tsx
@@ -1,18 +1,18 @@
 import Script from "next/script";
 
 const CustomScripts = () => {
-    return (
-        <>
-            {/*===========GOOGLE ANALYTICS==============*/}
-            <Script
-                strategy='afterInteractive'
-                src={`https://www.googletagmanager.com/gtag/js?id=G-503VYT6TSX`}
-            />
-            <Script
-                id='gtag-init'
-                strategy='afterInteractive'
-                dangerouslySetInnerHTML={{
-                    __html: `
+  return (
+    <>
+      {/*===========GOOGLE ANALYTICS==============*/}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-503VYT6TSX`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
                       window.dataLayer = window.dataLayer || [];
                       function gtag(){dataLayer.push(arguments);}
                       gtag('js', new Date());
@@ -20,15 +20,18 @@ const CustomScripts = () => {
                       page_path: window.location.pathname,
                       });
                     `,
-                }}
-            />
+        }}
+      />
 
-            {/*===========ADSENSE=============*/}
-            <meta name="google-adsense-account" content="ca-pub-3003249944965153"/>
-
-        </>
-    );
+      {/*===========ADSENSE=============*/}
+      <meta name="google-adsense-account" content="ca-pub-3768083860941636" />
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3768083860941636"
+        crossorigin="anonymous"
+      ></Script>
+    </>
+  );
 };
-
 
 export default CustomScripts;
